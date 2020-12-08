848deebdc84802788e5c1b58ce18804af7792aff
dicomtools <daniellafontaine@yahoo.ca>
2020-12-08 13:49:38 -0500

    Removed persistent variables

--- TriDFusion_core/3D/setMIPCallback.m
+++ TriDFusion_core/3D/setMIPCallback.m
@@ -26,9 +26,6 @@ function setMIPCallback(hObject, ~)
 % 
 % You should have received a copy of the GNU General Public License
 % along with TriDFusion.  If not, see <http://www.gnu.org/licenses/>.
-
-    persistent pMipAlphaMap;
-    persistent pMipFusionAlphaMap;
     
     if numel(dicomBuffer('get')) && ...
        size(dicomBuffer('get'), 3) ~= 1
